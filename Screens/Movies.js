9f37781718cd5f6be319a574817b944128fd7520
CHO1116 <dudwnscho@naver.com>
2021-05-25 23:58:22 +0900

    API modify

--- Screens/Movies.js
+++ Screens/Movies.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { movieApi } from '../api';
 
-export default ({ navigation }) => (
-  <View style={{ flex: 1, backgroundColor: 'black' }}>
-    <Text>This Tab is about the Movies.</Text>
-    <Button title='Movie' onPress={() => navigation.navigate('Detail')} />
-  </View>
-);
+export default () => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    nowPlayingE: null,
+    popular: [],
+    popularE: null,
+    upcoming: [],
+    upcomingE: null,
+  });
+
+  const getData = async () => {
+    const [nowPlaying, nowPlayingE] = await movieApi.nowPlaying();
+    const [popular, popularE] = await movieApi.popular();
+    const [upcoming, upcomingE] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      nowPlayingE,
+      popular,
+      popularE,
+      upcoming,
+      upcomingE,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View style={{ flex: 1, backgroundColor: 'black' }}>
+      <Text style={{ color: 'white' }}>{movies.nowPlaying?.length}</Text>
+    </View>
+  );
+};
--- api.js
+++ api.js
@@ -3,26 +3,40 @@ import axios from 'axios';
 const TMDB_KEY = 'acafe85e0d269668f1a23b10e425bc58';
 
 const makeRequest = (path, params) =>
-  axios.get('https://api.themoviedb.org/3${path}', {
+  axios.get('https://api.themoviedb.org/3{path}', {
     params: {
       ...params,
       api_key: TMDB_KEY,
     },
   });
 
+const getAnything = async (path, params = {}) => {
+  try {
+    const {
+      data: { results },
+      data,
+    } = await makeRequest(path, params);
+    console.log(data);
+    return [results || data, null];
+  } catch (e) {
+    console.log(e);
+    return [null, e];
+  }
+};
+
 export const movieApi = {
-  nowPlaying: () => makeRequest(),
-  popular: () => makeRequest(),
-  upcoming: () => makeRequest(),
-  search: (word) => makeRequest(),
-  movie: (id) => makeRequest(),
-  discover: () => makeRequest(),
+  nowPlaying: () => getAnything('/movie/now_playing'),
+  popular: () => getAnything('/movie/popular'),
+  upcoming: () => getAnything('/movie/upcoming', { region: 'kr' }),
+  search: (query) => getAnything('/search/movie', { query }),
+  movie: (id) => getAnything('/movie/{id}'),
+  discover: () => getAnything('/discover/movie'),
 };
 export const tvApi = {
-  today: () => makeRequest(),
-  thisWeek: () => makeRequest(),
-  topRated: () => makeRequest(),
-  popular: () => makeRequest(),
-  search: (word) => makeRequest(),
-  show: (id) => makeRequest(),
+  today: () => getAnything('/tv/airing_today'),
+  thisWeek: () => getAnything('/tv/on_the_air'),
+  topRated: () => getAnything('/tv/top_rated'),
+  popular: () => getAnything('/tv/popular'),
+  search: (query) => getAnything('/search/tv', { query }),
+  show: (id) => getAnything('/tv/{id}'),
 };
